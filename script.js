4ec1f2b77a2a6b80ddf7fa412b48a4d1b951c08c
eduardaferreir <105672394+eduardaferreir@users.noreply.github.com>
2024-06-03 08:19:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    enunciado: "Pergunta 1",
+    alternativas: [
+    "Alternativas 1", 
+    "Alternativa 2"]
+
+    "Pergunta 2"
+]
